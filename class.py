811ceacafffdcbd811ea3dfb49c429ca3af0bd3e
chychey <greenchy222@gmail.com>
2024-02-06 15:22:13 -0800

    commit

--- class.py
+++ class.py
@@ -9,7 +9,7 @@ class Organism:
     carbon_based = True
 
     def information(self):
-        msg = "\nName: ()\nSpecies: ()\nLegs: ()\nArms: ()\nDNA: ()\nOrigin: ()\nCarbon Based: ()".format(self.name,self.species,self.legs,self.arms,self.dna,self.origin,self.carbon_based)
+        msg = "Organsim: \nName: {}\nSpecies: {}\nLegs: {}\nArms: {}\nDNA: {}\nOrigin: {}\nCarbon Based: {}".format(self.name,self.species,self.legs,self.arms,self.dna,self.origin,self.carbon_based)
         return msg
 # child class instance
 class Human(Organism):
@@ -18,6 +18,10 @@ class Human(Organism):
     legs = 2
     arms = 2
     origin = 'Earth'
+
+    def information(self):
+        msg = "Human: \nName: {}\nSpecies: {}\nLegs: {}\nArms: {}\nDNA: {}\nOrigin: {}\nCarbon Based: {}".format(self.name,self.species,self.legs,self.arms,self.dna,self.origin,self.carbon_based)
+        return msg
 
     def ingenuity(self):
         msg = "\nCreates a deadly weapon using only a paper clip, chewing gum, and a roll of duct tape!"
