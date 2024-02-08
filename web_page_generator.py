c6b1fc1a82a6c83c1de3cf5748a0915698e74dce
chychey <greenchy222@gmail.com>
2024-02-08 15:43:15 -0800

    commit

--- web_page_generator.py
+++ web_page_generator.py
@@ -9,7 +9,7 @@ class ParentWindow(Frame):
         self.labelTxt = Label(text = "Enter custom text or click the defaulting HTML button")
         self.labelTxt.grid(row=0,column=0, padx=20, pady=(20,0))
         self.btn = Button(self.master, text="Default HTML Page", width=30, height=2, command=self.defaultHTML)
-        self.btn.grid(padx=(10,10) , pady=(10,10))
+        self.btn.grid(row=2, column=1, padx=(0,10) , pady=(0,10))
         self.txtEntry = Entry(self.master, font=("Helvetica", 12))
         self.txtEntry.grid(row=1, column=0, padx=(30,15), pady=(10,10), columnspan=3, sticky=W+E)
         self.btn = Button(self.master, text="Submit Custom Text", width=30, height=2, command=self.customHTML)
@@ -22,7 +22,6 @@ class ParentWindow(Frame):
         htmlFile.write(htmlContent)
         htmlFile.close()
         webbrowser.open_new_tab("index.html")
-        self.btn = Button(self.master, text="Default HTML Page", width=30, height=2, command=self.defaultHTML)
         #Import the required libraries
 
     def customHTML(self):
