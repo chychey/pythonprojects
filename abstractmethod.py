69f32afd97b812b462060dfb1572c4e57171aa38
chychey <greenchy222@gmail.com>
2024-02-07 19:05:16 -0800

    commit

--- abstractmethod.py
+++ abstractmethod.py
@@ -8,14 +8,14 @@ class car(ABC):
         def payment(self, amount):
             pass
 
-    class DebitCardPayment(car):
+class DebitCardPayment(car):
         #here we've defined how to implement the payment function from its parent paySlip class.
-        def payment(self,amount):
-            print('Your purchase amount of {} exceeded your $100 limit'.format(amount))
+    def payment(self,amount):
+        print('Your purchase amount of {} exceeded your $100 limit'.format(amount))
 
     obj = DebitCardPayment()
-    obj.paySlep("$400")
+    obj.paySlip("$400")
     obj.payment("$400")
 
-    Your purchase amount: $400
-    Your purchase amount of $400 exceeded your $100 limit
+  #  Your purchase amount: $400
+   # Your purchase amount of $400 exceeded your $100 limit
