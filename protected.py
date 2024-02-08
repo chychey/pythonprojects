f7c0fbd6dd31fdc4c494e62f411048c5d1d79c35
chychey <greenchy222@gmail.com>
2024-02-08 11:47:48 -0800

    commit

--- protected.py
+++ protected.py
@@ -1,17 +1,14 @@
-class Protected:
-   def __init__ (self):
-      self._protectedVar = 0
 
-obj = Protected()
-obj._protectedVar = 34
-print(obj.protectedVar)
+
 
 class Protected:
     def __init__(self):
+      self._protectedVar = 0
       self.__privateVar = 12
 
     def getPrivate(self):
       print(self.__privateVar)
+      self._protectedVar = 0
 
     def setPrivate(self, private):
       self.__privateVar = private
@@ -20,3 +17,5 @@ obj = Protected()
 obj.getPrivate()
 obj.setPrivate(24)
 obj.getPrivate()
+obj._protectedVar = 34
+print(obj._protectedVar)
