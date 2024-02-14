e5b5467f10810fe4558f8ed63b535eb9f3939e6b
chychey <greenchy222@gmail.com>
2024-02-14 12:35:49 -0800

    commit

--- techprojects/src/mainapp/mainapp/urls.py
+++ techprojects/src/mainapp/mainapp/urls.py
@@ -24,6 +24,11 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.contrib import admin
+from django.urls import path
+from . import views 
+
 urlpatterns = [
     path('', views.home, name="home"),
     path('admin/', admin.site.urls),
