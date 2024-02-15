41d3735dd1dd816ecfceb1c515cfc0be26e64301
chychey <greenchy222@gmail.com>
2024-02-15 15:18:57 -0800

    coomit

--- techprojects/src/mainapp/products/views.py
+++ techprojects/src/mainapp/products/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 from .forms import ProductForm
 from .models import Product
+
+
 def admin_console(request):
     products = Product.objects.all()
     return render(request, 'products/products_page.html', {'products': products})
