525d789623c370ec33956a7be14196425d33de3c
chychey <greenchy222@gmail.com>
2024-02-16 11:09:00 -0800

    commit

--- techprojects/src/mainapp/products/views.py
+++ techprojects/src/mainapp/products/views.py
@@ -24,3 +24,34 @@ def details(request, pk):
         return render(request, 'products/present_product.html', {'form': form})
 
 # Create your views here.
+def delete(request, pk):
+    pk = int(pk)
+    item = get_object_or_404(Product, pk=pk)
+    if request.method == 'POST':
+        item.delete()
+        return redirect('admin_console')
+    context = {"item": item, }
+    return render(request, "products/confirmDelete.html", context)
+
+
+def confirmed(request):
+    if request.method == 'POST':
+        form = ProductForm(request.POST or None)
+        if form.is_valid():
+            form.delete()
+            return redirect('admin_console')
+    else:
+        return redirect('admin_console')
+
+def createRecord(request):
+    form = ProductForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('admin_console')
+    else:
+        print(form.errors)
+        form = ProductForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'products/createRecord.html', context)
